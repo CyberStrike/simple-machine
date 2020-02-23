e6a52e9557ba1266e2ca2ddd2d1824017d710e84
Chris Scott <cyberstrike@gmail.com>
2020-02-23 18:12:10 -0500

    Card And Main

---
+++ src/components/MainView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="Main">
+    <card/>
+  </div>
+</template>
+
+<script>
+import Card from "./Card";
+
+export default {
+  name: "Main",
+  components: { Card }
+};
+</script>
